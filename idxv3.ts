2dc3f04c8ec77f2e0555ade1c75f4e228ee32433
jacoblang11 <jacoblang72@comcast.net>
2025-02-21 13:17:32 -0500

    think i completed the script and it is ready for testing

--- idxv3.ts
+++ idxv3.ts
@@ -4,26 +4,87 @@ import path from "path";
 import os from "os";
 import chokidar from "chokidar";
 import fs from "fs";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCVaAByzO-nOFY6XaVXLgTYHiGxlUWkZII",
+  authDomain: "popshap-kiosks-dev.firebaseapp.com",
+  projectId: "popshap-kiosks-dev",
+  storageBucket: "popshap-kiosks-dev.appspot.com",
+  messagingSenderId: "827903591200",
+  appId: "1:827903591200:web:a3a3dd97574a553fb64d45",
+  measurementId: "G-X3G2G8FZJ4",
+};
 
-function executeScript(inputFile: string) {
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app, "popshap-kiosks-dev.appspot.com");
+const map = new Map();
+const uploadMedia = async (
+  refPath: string,
+  file: Blob,
+  type: string
+): Promise<Record<string, string>> => {
+  const storageRef = ref(storage, refPath);
+  return new Promise(function (resolve, reject) {
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.clear();
+        console.log(`Upload is ${progress.toFixed(2)}% done`);
+      },
+      (error) => {
+        reject(error);
+      },
+      async () => {
+        await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          let data = { src: downloadURL, type: type };
+          resolve(data);
+        });
+      }
+    );
+  });
+};
+
+function executeScript(inputFile: string, _refPath: string) {
   const env = process.env.ENVIRONMENT || "DEV";
   const fileOutput = (file: string) =>
     env === "DEV" ? `tmp/${file}` : path.join(os.tmpdir(), file);
-  const watcher = chokidar.watch(env === 'DEV' ? 'tmp' : os.tmpdir(), {persistent: true})
+  const watcher = chokidar.watch(env === "DEV" ? "tmp" : os.tmpdir(), {
+    persistent: true,
+    ignored: (path, stats) => stats?.isFile() && !path.endsWith(".ts"),
+  });
   try {
     let int = 0;
     const iid = setInterval(() => {
       int++;
     }, 1000);
-    watcher.on("add", async(fileName) => {
-      console.log(`new file added ${fileName}`);
+    watcher.on("add", async (fileName) => {
+      const buffer = await fs.promises.readFile(fileName);
+      const download_url = await uploadMedia(
+        _refPath,
+        new Blob([buffer], { type: "MP2T" }),
+        "MP2T"
+      );
+      map.set(fileName, download_url);
       fs.unlink(fileName, (err) => {
         if (err) {
-          console.error('Error during file conversion in unlinking in tmp directory: ' + err);
+          console.error(
+            "Error during file conversion in unlinking in tmp directory: " + err
+          );
         }
-      })
-    })
+      });
+    });
     const ffmpeg = spawn(
       "ffmpeg",
       [
@@ -71,19 +132,30 @@ function executeScript(inputFile: string) {
       clearInterval(iid);
       console.error("Process closed with error: ", err);
     });
-    ffmpeg.on("close", async(data) => {
+    ffmpeg.on("close", async (data) => {
       clearInterval(iid);
       const minutes = Math.floor(int / 60);
       const seconds = int - minutes * 60;
       console.log("elapsed time: ", int + " second(s)");
       console.log("elapsed time: ", minutes + " min " + seconds + " second(s)");
       console.log("Process closed with no error: ", data);
-      await watcher.close().then(() => console.log('file watcher closed'));
+      await watcher.close().then(() => console.log("file watcher closed"));
+      const fileData = [];
+      for (const entry of map.entries()) {
+        fileData.push(entry);
+      }
+      await new Promise((resolve, reject) => {
+        fs.writeFile("downloadurl.txt", fileData.join(","), (err) => {
+          if (err) reject(err);
+          else resolve;
+        });
+      });
+      console.log(map);
     });
   } catch (err) {
     console.error(err);
     return err;
   }
 }
-
-executeScript(process.argv.at(-1));
+const refPath = "jacob-cache-test/media";
+executeScript(process.argv.at(-1), refPath);

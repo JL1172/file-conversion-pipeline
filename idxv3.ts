5d8a9ef197fc58103d6f056ecb4f362384744aa4
jacoblang11 <jacoblang72@comcast.net>
2025-02-21 18:54:07 -0500

    works until you try to upload

--- idxv3.ts
+++ idxv3.ts
@@ -27,40 +27,24 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app, "popshap-kiosks-dev.appspot.com");
 const map = new Map();
+
 const uploadMedia = async (
   refPath: string,
   file: Blob,
   type: string
 ): Promise<Record<string, string>> => {
   const storageRef = ref(storage, refPath);
-  return new Promise(function (resolve, reject) {
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.clear();
-        console.log(`Upload is ${progress.toFixed(2)}% done`);
-      },
-      (error) => {
-        reject(error);
-      },
-      async () => {
-        await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          let data = { src: downloadURL, type: type };
-          resolve(data);
-        });
-      }
-    );
-  });
+  const uploadTaskSnapshot = await uploadBytesResumable(storageRef, file);
+  const downloadURL = await getDownloadURL(uploadTaskSnapshot.ref);
+
+  return { src: downloadURL, type };
 };
 
 async function executeScript(inputFile: string, _refPath: string) {
   const env = process.env.ENVIRONMENT || "DEV";
   const startCPU = process.cpuUsage();
   if (inputFile.split(".").at(-1) !== "mp4") {
-    throw new Error('Unexpected file type');
+    throw new Error("Unexpected file type");
   }
   const fileOutput = (file: string) =>
     env === "DEV" ? `tmp/${file}` : path.join(os.tmpdir(), file);
@@ -70,23 +54,30 @@ async function executeScript(inputFile: string, _refPath: string) {
   });
   try {
     let int = 0;
+    let activeUpload = 0;
     const iid = setInterval(() => {
       int++;
     }, 1000);
     watcher.on("add", async (fileName) => {
-      const buffer = await fs.promises.readFile(fileName);
-      const download_url = await uploadMedia(
-        _refPath,
-        new Blob([buffer], { type: "MP2T" }),
-        "MP2T"
-      );
-      map.set(fileName.split("/")?.at(-1), download_url);
-      fs.unlink(fileName, (err) => {
-        if (err) {
-          console.error(
-            "Error during file conversion in unlinking in tmp directory: " + err
-          );
-        }
+      activeUpload++;
+      // const buffer = await fs.promises.readFile(fileName);
+      // const download_url = await uploadMedia(
+      //   _refPath,
+      //   new Blob([buffer], { type: "MP2T" }),
+      //   "MP2T"
+      // );
+      // if (!download_url?.src) process.exit(1);
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      map.set(fileName.split("/")?.at(-1), "download_url?.src");
+      await new Promise((resolve, reject) => {
+        fs.unlink(fileName, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve("");
+          }
+        });
+        activeUpload--;
       });
     });
     const ffmpeg = spawn(
@@ -127,58 +118,79 @@ async function executeScript(inputFile: string, _refPath: string) {
       { stdio: ["pipe", "pipe", "pipe"] }
     );
     ffmpeg.stdout.on("data", (data) => {
-      console.log(data.toString());
+      // console.log(data.toString());
     });
     ffmpeg.stderr.on("data", (data) => {
-      console.log(data.toString());
+      // console.log(data.toString());
     });
     ffmpeg.on("error", (err) => {
       clearInterval(iid);
       console.error("Process closed with error: ", err);
     });
+
     ffmpeg.on("close", async (data) => {
-      await watcher.close().then(() => console.log("file watcher closed"));
-      const fileData = [];
-      for (const entry of map.entries()) {
-        fileData.push(entry);
-      }
-      const data_read: string = await new Promise((resolve, reject) => {
-        fs.readFile(fileOutput("high.m3u8"), {encoding: "utf-8"}, (err,data) => {
-          if (err) reject(err);
-          else resolve(data);
-        } )
-      })
-      const dataParse1 = data_read?.split("\n");
-      const len: number = dataParse1?.length;
-      for (let i: number = 0; i < len; i++) {
-        if (/high_/i.test(dataParse1[i])) {
-          const name = dataParse1?.[i]?.trim();
-          dataParse1[i] = map.get(name);
+      const interval_id = setInterval(async () => {
+        if (activeUpload === 0) {
+          clearInterval(interval_id);
+          await watcher.close().then(() => console.log("file watcher closed"));
+          const fileData = [];
+          for (const entry of map.entries()) {
+            fileData.push(entry);
+          }
+          const data_read: string = await new Promise((resolve, reject) => {
+            fs.readFile(
+              fileOutput("high.m3u8"),
+              { encoding: "utf-8" },
+              (err, data) => {
+                if (err) reject(err);
+                else resolve(data);
+              }
+            );
+          });
+          const dataParse1 = data_read?.split("\n");
+          const len: number = dataParse1?.length;
+          for (let i: number = 0; i < len; i++) {
+            if (/^high_\d{3}\.ts$/i.test(dataParse1[i])) {
+              const name = dataParse1?.[i]?.trim();
+              console.log("map get ->", map.get(name));
+              dataParse1[i] = map.get(name);
+            }
+          }
+          await new Promise((resolve, reject) => {
+            const dataToInsertIntoManifest = dataParse1.join("\n");
+            fs.writeFile(
+              fileOutput("high.m3u8"),
+              dataToInsertIntoManifest,
+              { encoding: "utf-8" },
+              (err) => {
+                if (err) reject(err);
+                else resolve("completed");
+              }
+            );
+          });
+          clearInterval(iid);
+          const minutes = Math.floor(int / 60);
+          const seconds = int - minutes * 60;
+          console.log("elapsed time: ", int + " second(s)");
+          console.log(
+            "elapsed time: ",
+            minutes + " min " + seconds + " second(s)"
+          );
+          const endCpu = process.cpuUsage(startCPU);
+          const totalCPUTime = (endCpu.user + endCpu.system) / 1e6;
+          console.log(
+            "High level estimate of cpu usage ->",
+            (totalCPUTime / int) * 100 + "%"
+          );
+          console.log("Process closed with no error: ", data);
         }
-      }
-      await new Promise((resolve ,reject) => {
-        const dataToInsertIntoManifest = dataParse1.join("\n");
-        fs.writeFile(fileOutput("high.m3u8"), dataToInsertIntoManifest, {encoding: "utf-8"}, (err) => {
-          if (err) reject(err);
-          else resolve('completed');
-        })
-      })
-      clearInterval(iid);
-      const minutes = Math.floor(int / 60);
-      const seconds = int - minutes * 60;
-      console.log("elapsed time: ", int + " second(s)");
-      console.log("elapsed time: ", minutes + " min " + seconds + " second(s)");
-      const endCpu = process.cpuUsage(startCPU);
-      const totalCPUTime = (endCpu.user + endCpu.system) / 1e6;
-      console.log("High level estimate of cpu usage ->", ((totalCPUTime / int) * 100) + "%");
-      console.log("Process closed with no error: ", data);
+      }, 1000);
     });
-    } catch (err) {
-      console.error(err);
+  } catch (err) {
+    console.error(err);
     return err;
   }
 }
+
 const refPath = "jacob-cache-test/media";
 executeScript(process.argv.at(-1), refPath);
-
-
